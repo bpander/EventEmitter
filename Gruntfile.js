340d2d907919f3181eb4c34eb88636d7609bd774
Bradley Anderson <brad.anderson87@gmail.com>
2015-12-23 16:24:29 -0600

    Adds `dist` grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,32 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         jsdoc2md: {
-            readme: {
+            dist: {
                 src: 'src/EventEmitter.js',
                 dest: 'README.md'
+            }
+        },
+
+        copy: {
+            dist: {
+                files: {
+                    'dist/EventEmitter.js': ['src/EventEmitter.js']
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    'dist/EventEmitter.min.js': ['dist/EventEmitter.js']
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-jsdoc-to-markdown')
-    grunt.registerTask('default', 'jsdoc2md')
+    grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['copy:dist', 'uglify:dist', 'jsdoc2md:dist']);
 };
